744ce725842420c402266e11cf542f54c3f6ea09
habibaragab324@gmail.com <habibaragab324@gmail.com>
2023-10-19 19:44:23 -0700

    add

--- fun1.c
+++ fun1.c
@@ -6,7 +6,7 @@
  * @new_node: Pointer to the new node.
  * @ln: Interger representing the line number of of the opcode.
  */
-void add_to_stack(stack_t **new_node, _attribute_((unused))unsigned int ln)
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
 	stack_t *tmp;
 
