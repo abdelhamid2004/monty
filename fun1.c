2a3735ac0335dce358c036f9d82526ba9071bbf5
root <root@DESKTOP-5OPKFDG>
2023-10-20 05:41:47 +0300

    acc

--- fun1.c
+++ fun1.c
@@ -6,21 +6,21 @@
  * @new_node: Pointer to the new node.
  * @ln: Interger representing the line number of of the opcode.
  */
-void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+void add_to_stack(stack_t **new_node, _attribute_((unused))unsigned int ln)
 {
-        stack_t *tmp;
+	stack_t *tmp;
 
-        if (new_node == NULL || *new_node == NULL)
-                exit(EXIT_FAILURE);
-        if (head == NULL)
-        {
-                head = *new_node;
-                return;
-        }
-        tmp = head;
-        head = *new_node;
-        head->next = tmp;
-        tmp->prev = head;
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
 }
 
 
@@ -31,17 +31,17 @@ void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
  */
 void print_stack(stack_t **stack, unsigned int line_number)
 {
-        stack_t *tmp;
+	stack_t *tmp;
 
-        (void) line_number;
-        if (stack == NULL)
-                exit(EXIT_FAILURE);
-        tmp = *stack;
-        while (tmp != NULL)
-        {
-                printf("%d\n", tmp->n);
-                tmp = tmp->next;
-        }
+	(void) line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
 
 /**
@@ -51,16 +51,16 @@ void print_stack(stack_t **stack, unsigned int line_number)
  */
 void pop_top(stack_t **stack, unsigned int line_number)
 {
-        stack_t *tmp;
+	stack_t *tmp;
 
-        if (stack == NULL || *stack == NULL)
-                more_err(7, line_number);
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_number);
 
-        tmp = *stack;
-        *stack = tmp->next;
-        if (*stack != NULL)
-                (*stack)->prev = NULL;
-        free(tmp);
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
 }
 
 /**
@@ -70,7 +70,7 @@ void pop_top(stack_t **stack, unsigned int line_number)
  */
 void print_top(stack_t **stack, unsigned int line_number)
 {
-        if (stack == NULL || *stack == NULL)
-                more_err(6, line_number);
-        printf("%d\n", (*stack)->n);
+	if (stack == NULL || *stack == NULL)
+		more_err(6, line_number);
+	printf("%d\n", (*stack)->n);
 }
